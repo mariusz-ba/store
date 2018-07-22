d7761af52904cc784fbd496b057504d666688ac0
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-23 00:34:04 +0200

    Remove console logs

--- server/services/ProductService.js
+++ server/services/ProductService.js
@@ -4,17 +4,14 @@ class ProductService {
   }
 
   getProducts = async () => {
-    console.log('ProductService: getting products');
     return this.Product.find({});
   }
 
   getProductById = async (productId) => {
-    console.log(`ProductService: getting product (${productId})`);
     return this.Product.findById(productId);
   }
 
   saveProduct = async (product) => {
-    console.log('ProductService: creating new product');
     await product.save();
     return product;
   }
