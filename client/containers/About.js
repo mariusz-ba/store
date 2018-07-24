f28e066f4eeb36990b6980d3a219bff0a7f29fb0
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-24 17:08:51 +0200

    Create about page

--- client/containers/About.js
+++ client/containers/About.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { 
+  Spacer,
+  Wrapper
+} from '../components/layout';
+import Slider from '../components/slider';
+import {
+  Title,
+  Description,
+  Pictures,
+  Picture
+} from './styles';
 
-export default () => {
-  return (
-    <h1>About</h1>
-  )
+export default class About extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Slider images={['https://images.unsplash.com/photo-1483900436872-daac2aca11a2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c3fbd256649f42665c568c0640006ad4&auto=format&fit=crop&w=1350&q=80']}/>
+        <Spacer>&#10699;</Spacer>
+        <Title>AN INTRODUCTION</Title>
+        <Description>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a sem purus. Proin nec pulvinar elit. In hac habitasse platea dictumst. Aenean vel facilisis ligula.</p>
+          <p>Aliquam eget elit sagittis, mattis tellus non, commodo nisi. Mauris convallis tristique finibus. Morbi eu sapien nulla. Mauris vitae tortor pulvinar, eleifend augue dapibus, dapibus erat.</p>
+          <p>Nulla quis arcu lorem. Vestibulum feugiat rhoncus aliquet. Aliquam porttitor facilisis orci, sit amet vestibulum quam rhoncus sit amet. In vitae massa sollicitudin, ornare quam ut, dapibus sem.</p>
+          <p>The brand identity was created by <a href="https://mariusz-ba.github.io/portfolio">MARIUSZ BARAN</a> in Cracow.</p>
+        </Description>
+        <Pictures>
+          <Picture src="https://images.unsplash.com/photo-1516886635086-2b3c423c0947?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9d541acfcafecad7e20b05ffa23b87dd&auto=format&fit=crop&w=701&q=80"/>
+          <Picture src="https://images.unsplash.com/photo-1487537708572-3c850b5e856e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4d58daea6773f3ca15788b46f3277a4e&auto=format&fit=crop&w=1352&q=80"/>
+        </Pictures>
+      </Wrapper>
+    )
+  }
 }
---
+++ client/containers/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  text-align: center;
+  letter-spacing: .1rem;
+`
+
+export const Description = styled.div`
+  text-align: center;
+
+  p {
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 1rem 0;
+    line-height: 1.5;
+  }
+
+  a {
+    color: #325ac8;
+    text-decoration: underline;
+    letter-spacing: .1rem;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+`
+
+export const Pictures = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem 0;
+`
+
+export const Picture = styled.img`
+  width: 600px;
+  height: 400px;
+  margin: 1rem;
+  object-fit: cover;
+`
