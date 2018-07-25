9181ed8dda7da7e7b40346b23c629ef7d211d440
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-25 19:16:39 +0200

    Allow category service to update and delete categories

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -20,4 +20,15 @@ router.post('/', async (req, res) => {
   res.status(200).json(savedCategory);
 })
 
+router.put('/:id', async (req, res) => {
+  const category = await categoryService.updateCategory(req.params.id, { ...req.body });
+  res.status(200).json(category);
+})
+
+router.delete('/:id', async (req, res) => {
+  const deleted = await categoryService.deleteCategory(req.params.id);
+  // Get all products with this category and set category to null
+  res.status(200).json(deleted);
+})
+
 export default router;
--- server/services/CategoryService.js
+++ server/services/CategoryService.js
@@ -15,6 +15,14 @@ class CategoryService {
     await category.save();
     return category;
   }
+
+  updateCategory = async (categoryId, category) => {
+    return this.Category.findOneAndUpdate({ _id: categoryId }, { $set: { ...category }}, { new: true });
+  }
+
+  deleteCategory = async (categoryId) => {
+    return this.Category.deleteOne({ _id: categoryId });
+  }
 }
 
 export default CategoryService;
