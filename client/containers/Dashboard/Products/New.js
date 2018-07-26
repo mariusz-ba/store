ad91b430ca44713e185aa133db5e4d0abef48a98
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-26 22:32:49 +0200

    Provide ability of adding new products on new product page

--- client/containers/Dashboard/Products/New.js
+++ client/containers/Dashboard/Products/New.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCategories } from 'actions/categoriesActions';
+import { createProduct } from 'actions/productsActions';
 
-export default class ProductsNew extends Component {
+import ProductEditor from 'components/editors/ProductEditor';
+
+class ProductsNew extends Component {
+  componentDidMount() {
+    this.props.fetchCategories();
+  }
+
+  onSubmit = product => {
+    this.props.createProduct(product);
+  }
+
   render() {
+    const { categories } = this.props.categories;
+
     return (
-      <h1>Create new product</h1>
+      <div>
+        <h1>Create product</h1>
+        <ProductEditor
+          categories={Object.values(categories)}
+          onSubmit={this.onSubmit}
+        />
+      </div>
     )
   }
 }
+
+const mapStateToProps = ({ categories }) => ({ categories });
+
+export default connect(mapStateToProps, { fetchCategories, createProduct })(ProductsNew);
