721228d066e54bdbae17699ca98c8aa06d49096d
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-26 22:34:35 +0200

    Display products on dashboard products page and provide ability to manage products

--- client/containers/Dashboard/Products/index.js
+++ client/containers/Dashboard/Products/index.js
@@ -1,9 +1,114 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Products extends Component {
+import { fetchSizes } from 'actions/sizesActions';
+import { 
+  fetchProducts, 
+  updateProduct,
+  deleteProduct,
+  createProductAvailability,
+  updateProductAvailability,
+  deleteProductAvailability
+} from 'actions/productsActions';
+import { fetchCategories } from 'actions/categoriesActions';
+
+import AvailabilityEditor from 'components/editors/AvailabilityEditor';
+import ProductEditor from 'components/editors/ProductEditor';
+
+import Expander from 'components/Expander';
+
+class Products extends Component {
+  componentDidMount() {
+    this.props.fetchSizes();
+    this.props.fetchProducts();
+    this.props.fetchCategories();
+  }
+
+  onSubmitProduct = (product) => {
+    // Update product in database
+    console.log(product);
+    this.props.updateProduct(product._id, product);
+  }
+
+  onSubmitAvailability = (availability) => {
+    // Update availability in database
+    console.log(availability);
+    const { _id, product, size, amount } = availability;
+    if(_id) {
+      console.log('updateProductAvailability');
+      this.props.updateProductAvailability(_id, size, amount);
+    } else {
+      console.log('createProductAvailability');
+      this.props.createProductAvailability(product, size, amount);
+    }
+  }
+
+  deleteProduct = (productId) => {
+    this.props.deleteProduct(productId);
+  }
+
+  deleteProductAvailability = (availabilityId, productId) => {
+    this.props.deleteProductAvailability(availabilityId, productId);
+  }
+
   render() {
+    const { sizes } = this.props.sizes;
+    const { products } = this.props.products;
+    const { categories } = this.props.categories;
+
     return (
-      <h1>Products</h1>
+      <div>
+        <ul>
+          { products &&
+            Object.values(products).map(product => (
+              <li key={product._id}>
+                <Expander
+                  head={(
+                    <div>
+                      <h1>{product.name}</h1>
+                      <p>{product.description}</p>
+                    </div>
+                  )}
+                  body={(
+                    <div>
+                      <button onClick={() => this.deleteProduct(product._id)}>Delete</button>
+                      <ProductEditor
+                        { ...product }
+                        categories={Object.values(categories)}
+                        onSubmit={this.onSubmitProduct}
+                      />
+                      {
+                        product.availability.map(availability => (
+                          <div>
+                            <AvailabilityEditor
+                              { ...availability }
+                              product={product._id}
+                              sizes={Object.values(sizes)}
+                              onSubmit={this.onSubmitAvailability}
+                            />
+                            <button onClick={() => this.deleteProductAvailability(availability._id, product._id)}>Delete</button>
+                          </div>
+                        ))
+                      }
+                      <AvailabilityEditor
+                        product={product._id}
+                        sizes={Object.values(sizes)}
+                        onSubmit={this.onSubmitAvailability}
+                      />
+                    </div>
+                  )}
+                />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     )
   }
 }
+
+const mapStateToProps = ({ products, sizes, categories }) => ({ products, sizes, categories });
+
+export default connect(mapStateToProps, { 
+  fetchProducts, updateProduct, deleteProduct, createProductAvailability, updateProductAvailability, deleteProductAvailability, 
+  fetchCategories, fetchSizes })(Products);
