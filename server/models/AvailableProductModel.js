8b94c04e747a6c060eda8ac79f63cb565964e9bb
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-27 13:27:08 +0200

    Change available product schema

--- server/models/AvailableProductModel.js
+++ server/models/AvailableProductModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 // AvailableProduct schema
 const AvailableProduct = new Schema({
   product: { type: Schema.Types.ObjectId, required: true, ref: 'Product'},
-  size: { type: Schema.Types.Object, required: true, ref: 'Size'},
+  size: { type: Schema.Types.ObjectId, required: true, ref: 'Size'},
   amount: { type: Number, required: true, default: 0}
 })
 
