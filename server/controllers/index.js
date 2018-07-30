bd01a900c893c216b2ef4f0b4b120c329ee5d7a3
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-30 18:07:20 +0200

    Export services and controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,14 @@ import productController from './productController';
 import categoryController from './categoryController';
 import sizeController from './sizeController';
 import availableProductController from './availableProductController';
+import paymentController from './paymentController';
+import deliveryController from './deliveryController';
 
 module.exports = {
   productController,
   categoryController,
   sizeController,
-  availableProductController
+  availableProductController,
+  paymentController,
+  deliveryController
 }
--- server/services/index.js
+++ server/services/index.js
@@ -2,22 +2,30 @@ import ProductService from './ProductService';
 import CategoryService from './CategoryService';
 import SizeService from './SizeService';
 import AvailableProductService from './AvailableProductService';
+import PaymentService from './PaymentService';
+import DeliveryService from './DeliveryService';
 
 import { 
   Product,
   Category,
   Size,
-  AvailableProduct
+  AvailableProduct,
+  Payment,
+  Delivery
 } from '../models';
 
 const productService = new ProductService(Product);
 const categoryService = new CategoryService(Category);
 const sizeService = new SizeService(Size);
 const availableProductService = new AvailableProductService(AvailableProduct);
+const paymentService = new PaymentService(Payment);
+const deliveryService = new DeliveryService(Delivery);
 
 module.exports = {
   productService,
   categoryService,
   sizeService,
-  availableProductService
+  availableProductService,
+  paymentService,
+  deliveryService
 }
