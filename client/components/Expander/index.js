a4ee09bcdbf9e751bd7429005006654429c82fd0
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-01 18:04:42 +0200

    Change expander styles

--- client/components/Expander/index.js
+++ client/components/Expander/index.js
@@ -3,36 +3,45 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Expander = styled.div`
-  border-top: 1px solid rgba(0, 0, 0, .125);
-  border-left: 1px solid rgba(0, 0, 0, .125);
-  border-right: 1px solid rgba(0, 0, 0, .125);
+  box-shadow: 0 0 4px rgba(0, 0, 0, .125);
 `
 
 Expander.Head = styled.div`
-  display: grid;
-  grid-template-columns: auto 80px;
-  border-bottom: 1px solid rgba(0, 0, 0, .125);
+  color: #fff;
+  background: #1086ed;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 
 Expander.Head.Content = styled.div`
-  
+  small {
+    display: block;
+    margin-top: .5rem;
+    font-weight: bold;
+    color: rgb(0, 66, 114);
+  }
 `
 
 Expander.Head.Button = styled.button`
   border: 0;
   outline: 0;
   background: transparent;
-  border-left: 1px solid rgba(0, 0, 0, .125);
+  color: #fff;
+  padding: .5rem;
+  font-size: 1.5em;
 
   &:hover {
     cursor: pointer;
-    background: rgba(0, 0, 0, .02);
   }
 `
 
 Expander.Body = styled.div`
+  padding: 1rem;
   display: ${props => props.expanded ? 'block' : 'none'};
-  border-bottom: 1px solid rgba(0, 0, 0, .125);
+  color: #fff;
+  background: rgb(0, 126, 218);
 `
 
 export default class extends Component {
@@ -53,13 +62,13 @@ export default class extends Component {
     const { expanded } = this.state;
     const { head, body } = this.props;
 
-    const buttonContent = expanded ? 'Hide' : 'Expand';
+    const togglerClass = expanded ? 'fa-caret-up' : 'fa-caret-down';
 
     return (
       <Expander>
         <Expander.Head>
           <Expander.Head.Content>{head}</Expander.Head.Content>
-          <Expander.Head.Button onClick={this.toggle}>{buttonContent}</Expander.Head.Button>
+          <Expander.Head.Button onClick={this.toggle}><i className={`fas ${togglerClass}`}></i></Expander.Head.Button>
         </Expander.Head>
         <Expander.Body expanded={expanded}>{body}</Expander.Body>
       </Expander>
