461801d62b32db9e3a62d04b1fc1e4a1b7c5c0ba
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-02 17:13:55 +0200

    Style payments dashboard page

---
+++ client/containers/Dashboard/Payments/PaymentEditor.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Button from 'blocks/Button';
+import Form from 'blocks/Form';
+
+export default class DeliveryEditor extends Component {
+  state = {
+    name: this.props.name,
+    url: this.props.url,
+    price: String(this.props.price),
+    icon: this.props.icon
+  }
+
+  static propTypes = {
+    name: PropTypes.string,
+    url: PropTypes.string,
+    price: PropTypes.number,
+    icon: PropTypes.string,
+    onSubmit: PropTypes.func.isRequired
+  }
+
+  changeName = e => {
+    this.setState({ name: e.target.value });
+  }
+
+  changeUrl = e => {
+    this.setState({ url: e.target.value });
+  }
+
+  changePrice = e => {
+    this.setState({ price: e.target.value });
+  }
+
+  changeIcon = e => {
+    this.setState({ icon: e.target.value });
+  }
+
+  submit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.name, this.state.url, this.state.price, this.state.icon);
+  }
+
+  render() {
+    const { name, url, price, icon } = this.state;
+
+    return (
+      <Form>
+        <Form.Field>
+          <Form.Label>
+            Name
+            <Form.Input type="text" placeholder="Name" value={name} onChange={this.changeName}/>
+          </Form.Label>
+        </Form.Field>
+        <Form.Field>
+          <Form.Label>
+            Payment Url 
+            <Form.Input type="text" placeholder="Url" value={url} onChange={this.changeUrl}/>
+          </Form.Label>
+        </Form.Field>
+        <Form.Field>
+          <Form.Label>
+            Price
+            <Form.Input type="text" placeholder="Price" value={price} onChange={this.changePrice}/>
+          </Form.Label>
+        </Form.Field>
+        <Form.Field>
+          <Form.Label>
+            Icon
+            <Form.Input type="text" placeholder="Icon" value={icon} onChange={this.changeIcon}/>
+          </Form.Label>
+        </Form.Field>
+        <Button type="submit" onClick={this.submit}>Submit</Button>
+      </Form>
+    )
+  }
+}
--- client/containers/Dashboard/Payments/index.js
+++ client/containers/Dashboard/Payments/index.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { mapKeys } from 'lodash';
+
+import Expander from 'components/Expander';
+import Button from 'blocks/Button';
+import Form from 'blocks/Form';
+import { Title, Description } from 'blocks/Dashboard';
+import { Cards } from '../styles';
+import PaymentEditor from './PaymentEditor';
+
+import styled from 'styled-components';
+
+const NewPayment = Form.extend`
+  max-width: 400px;
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
+  background: #1086ed;
+  color: #fff;
+  & div,
+  & Button {
+    margin: 0 1rem;
+  }
+`
+NewPayment.Heading = styled.h3`
+  padding: 1rem 0;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: #fff;
+  background: rgb(0, 126, 218);
+`
 
 export default class Payments extends Component {
   state = {
-    payments: [],
+    payments: {},
     paymentName: '',
     paymentPrice: '',
     paymentUrl: '',
@@ -54,7 +83,7 @@ export default class Payments extends Component {
     const payment = response.data;
 
     this.setState({ 
-      payments: [ ...this.state.payments, payment ], 
+      payments: { ...this.state.payments, [payment._id]: payment }, 
       paymentName: '', 
       paymentUrl: '', 
       paymentPrice: '', 
@@ -69,6 +98,14 @@ export default class Payments extends Component {
     this.setState({ payments });
   }
 
+  changePayment = async (paymentId, name, url, price, icon) => {
+    const response = await axios.put(`/api/payments/${paymentId}`, { name, price, icon, url });
+    const payment = response.data;
+    this.setState({
+      payments: { ...this.state.payments, [payment._id]: payment }
+    })
+  }
+
   render() {
     const { 
       payments, 
@@ -80,25 +117,64 @@ export default class Payments extends Component {
 
     return (
       <div>
-        <h2>Payments</h2>
-        <ul>
+        <Title>Payment methods</Title>
+        <Description>Create new payment methods and edit existing ones.</Description>
+        <NewPayment>
+          <NewPayment.Heading>Create new payment</NewPayment.Heading>
+          <Form.Field>
+            <Form.Label>
+              Name
+              <Form.Input type="text" placeholder="Payment name" value={paymentName} onChange={this.changePaymentName}/>
+            </Form.Label>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label>
+              Url
+              <Form.Input type="text" placeholder="Payment url" value={paymentUrl} onChange={this.changePaymentUrl}/>
+            </Form.Label>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label>
+              Price
+              <Form.Input type="text" placeholder="Payment price" value={paymentPrice} onChange={this.changePaymentPrice}/>
+            </Form.Label>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label>
+              Icon
+              <Form.Input type="text" placeholder="Payment icon" value={paymentIcon} onChange={this.changePaymentIcon}/>
+            </Form.Label>
+          </Form.Field>
+          <Button onClick={this.submit}>Submit</Button>
+        </NewPayment>
+        <Cards>
           { payments &&
-            payments.map(payment => (
-              <li key={payment._id}>
-                <div>Name: {payment.name}, Url: {payment.url}, Price: {payment.price}, Icon: {payment.icon}</div>
-                <button onClick={() => this.deletePayment(payment._id)}>Delete</button>
-              </li>
+            Object.values(payments).map(payment => (
+              <Cards.Item key={payment._id}>
+                <Expander
+                  head={(
+                    <React.Fragment>
+                      <h3>{payment.name}</h3>
+                      <small>{payment.url}</small>
+                    </React.Fragment>
+                  )}
+                  body={(
+                    <div>
+                      <Button mode="danger" onClick={() => this.deletePayment(payment._id)}>Delete</Button>
+                      <PaymentEditor
+                        name={payment.name}
+                        url={payment.url}
+                        price={payment.price}
+                        icon={payment.icon}
+                        onSubmit={(name, url, price, icon) => this.changePayment(payment._id, name, url, price, icon)}
+                      />
+                    </div>
+                  )}
+                />
+              </Cards.Item>
             ))
           }
-        </ul>
-        <div>
-          <h2>Create new payment</h2>
-          <input type="text" placeholder="Payment name" value={paymentName} onChange={this.changePaymentName}/>
-          <input type="text" placeholder="Payment url" value={paymentUrl} onChange={this.changePaymentUrl}/>
-          <input type="text" placeholder="Payment price" value={paymentPrice} onChange={this.changePaymentPrice}/>
-          <input type="text" placeholder="Payment icon" value={paymentIcon} onChange={this.changePaymentIcon}/>
-          <button type="submit" onClick={this.submit}>Submit</button>
-        </div>
+        </Cards>
       </div>
     )
   }
