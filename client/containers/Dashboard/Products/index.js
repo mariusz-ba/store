34f0143e5f8301152bd3a34366a4426e788fc77f
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-06 18:06:03 +0200

    Create link to new product page on products list page

--- client/containers/Dashboard/Products/index.js
+++ client/containers/Dashboard/Products/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchSizes } from 'actions/sizesActions';
@@ -6,8 +7,17 @@ import { fetchProducts, deleteProduct } from 'actions/productsActions';
 import { fetchCategories } from 'actions/categoriesActions';
 
 import ProductsTable from 'components/ProductsTable';
-
+import Button from 'blocks/Button';
 import { Title, Description } from 'blocks/Dashboard';
+
+const NewProductLink = Button.withComponent(Link).extend`
+  font-size: 1rem;
+  float: right;
+  text-decoration: none;
+  i {
+    margin-right: 1rem;
+  }
+`;
 
 class Products extends Component {
   componentDidMount() {
@@ -27,7 +37,10 @@ class Products extends Component {
 
     return (
       <div>
-        <Title>Products</Title>
+        <Title>
+          Products
+          <NewProductLink mode="primary" to="/dashboard/products/new"><i className="fas fa-plus"></i>New product</NewProductLink>
+        </Title>
         <Description>Use list below to search and manage products</Description>
         <ProductsTable 
           products={Object.values(products)} 
